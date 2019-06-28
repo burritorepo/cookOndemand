86ec5152ff720a4408db83291a718d38b826201c
JaimDelmar <j.delmar.dan@gmail.com>
2019-06-28 15:47:16 -0500

    webpack reloaded

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -25,35 +25,17 @@ module.exports = {
         ]
       },
       {
-        test: /\.less$/,
-        use: [{
-          loader: 'style-loader',
-        }, {
-          loader: 'css-loader',
-        }, {
-          loader: 'less-loader',
-          options: {
-            modifyVars: {
-              'primary-color': '#1DA57A',
-              'link-color': '#1DA57A',
-              'border-radius-base': '2px',
-              'hack': `true; @import "your-less-file-path.less";`,
-            },
-            javascriptEnabled: true,
-          },
-        },
-        {
-          test: /\.(png|jpg|gif)$/i,
-          use: [
-            {
-              loader: 'url-loader',
-              options: {
-                limit: 8192
-              }
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
             }
-          ]
-        }
-        ],
-      }]
+          }
+        ]
+      }
+    ],
   }
 }
+
