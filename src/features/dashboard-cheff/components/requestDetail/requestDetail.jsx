1df5f9aa104245347d10ca276d727ff1b7c1be38
tecsup <tecsup@gmail.com>
2019-06-28 16:44:01 -0500

    it added changes

--- src/features/dashboard-cheff/components/requestDetail/requestDetail.jsx
+++ src/features/dashboard-cheff/components/requestDetail/requestDetail.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
-import { Breadcrumb, Tabs } from 'antd';
+import { Breadcrumb, Icon, Button, Row, Col } from 'antd';
 
 function callback(key) {
   console.log(key);
@@ -14,7 +14,7 @@ class DashboardRequestDetail extends Component {
   render() {
     const {
       match: { params: { id } },
-      match: { params: { user } } 
+      match: { params: { user } }
     } = this.props;
     return (
       <div className="view view-request-detail">
@@ -32,6 +32,33 @@ class DashboardRequestDetail extends Component {
         <br />
         <h1 className="title c-primary">Solicitud de {id}</h1>
         <br />
+        <div className="content">
+          <h2>Solicitud 22/06/19</h2>
+          <hr />
+          <br />
+          <Row gutter={16}>
+            <Col span={12}>
+              <p>
+                <Icon type="check" className="c-primary" />
+                <span className="c-primary">Cantidad de personas</span>
+              </p>
+              <p><b>13 - 20</b></p>
+            </Col>
+            <Col span={12}>
+              <p>
+                <Icon type="check" className="c-primary" />
+                <span className="c-primary">Tipo de cocina</span>
+              </p>
+              <p><b>Cocina a gas</b></p>
+            </Col>
+          </Row>
+          <br/>
+          <hr/>
+          <br/>
+          <div className="text-center">
+            <Button type="primary">Tomar Solicitud</Button>
+          </div>
+        </div>
       </div>
     )
   }
