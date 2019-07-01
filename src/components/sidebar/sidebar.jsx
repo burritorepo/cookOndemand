367b5922a0b25b6bc5f82d01a132f7d38e200f29
JaimDelmar <j.delmar.dan@gmail.com>
2019-07-01 15:23:52 -0500

    sidebar style

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,21 +1,31 @@
 import React from "react";
-import { NavLink } from 'react-router-dom';
-import { Layout, Menu } from 'antd';
-import './sidebar.scss';
+import { NavLink } from "react-router-dom";
+import { Layout, Menu } from "antd";
+import "./sidebar.scss";
 
 function ItemUser() {
   return (
     <React.Fragment>
       <Menu>
         <Menu.Item>
-          <NavLink to="/user/home" activeClassName="is-active ant-menu-item-selected">Inicio</NavLink>
+          <NavLink
+            to="/user/home"
+            activeClassName="is-active ant-menu-item-selected"
+          >
+            Inicio
+          </NavLink>
         </Menu.Item>
         <Menu.Item>
-          <NavLink to="/user/request" activeClassName="is-active ant-menu-item-selected">Solicitudes</NavLink>
+          <NavLink
+            to="/user/request"
+            activeClassName="is-active ant-menu-item-selected"
+          >
+            Solicitudes
+          </NavLink>
         </Menu.Item>
       </Menu>
     </React.Fragment>
-  )
+  );
 }
 
 function ItemCheff() {
@@ -23,35 +33,33 @@ function ItemCheff() {
     <React.Fragment>
       <Menu>
         <Menu.Item>
-          <NavLink to="/cheff/home" activeClassName="is-active ant-menu-item-selected">Inicio</NavLink>
+          <NavLink
+            to="/cheff/home"
+            activeClassName="is-active ant-menu-item-selected"
+          >
+            Inicio
+          </NavLink>
         </Menu.Item>
         <Menu.Item>
-          <NavLink to="/cheff/request" activeClassName="is-active ant-menu-item-selected">Solicitudes</NavLink>
-        </Menu.Item>
-        <Menu.Item>
-          <NavLink to="/cheff/dishes" activeClassName="is-active ant-menu-item-selected">Agregar Plato</NavLink>
-        </Menu.Item>
-        <Menu.Item>
-          <NavLink to="/cheff/menu" activeClassName="is-active ant-menu-item-selected">Menú</NavLink>
+          <NavLink
+            to="/cheff/request"
+            activeClassName="is-active ant-menu-item-selected"
+          >
+            Solicitudes
+          </NavLink>
         </Menu.Item>
       </Menu>
     </React.Fragment>
-  )
+  );
 }
 
 function Sidebar(props) {
   const { children } = props;
   return (
     <Layout.Sider className="sidebar">
-      {(sessionStorage.getItem('type') === 'user') ? (
-        <ItemUser />
-      ) : (
-          <ItemCheff />
-        )}
+      {sessionStorage.getItem("type") === "user" ? <ItemUser /> : <ItemCheff />}
     </Layout.Sider>
-  )
+  );
 }
 
-export {
-  Sidebar
-}
+export { Sidebar };
